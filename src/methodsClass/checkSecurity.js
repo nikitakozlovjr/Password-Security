797c1d5265d5a985c6ce76b1e2701d388772ddf8
nikitakozlovjr <nikita3726898@gmail.com>
2024-01-03 01:09:45 +0300

    Написали функцию, которая выполняет работу метода check

--- src/methodsClass/checkSecurity.js
+++ src/methodsClass/checkSecurity.js
@@ -1,3 +1,12 @@
+import paramCheck from '../param/paramCheck.js';
+import paramReliability from '../param/paramReliability.js';
+
 export default (password) => {
-    console.log(password);
+    if(password.length === 0) {
+        return 'Enter the password!';
+    }
+
+    const resultChecks = Object.values(paramCheck).map((check) => check.test(password));
+    const levelCheck = resultChecks.filter((resultCheck) => resultCheck === true).length;
+    return `${paramReliability[levelCheck]} level of password security`;
 };
